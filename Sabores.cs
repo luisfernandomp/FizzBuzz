c7860f06611f08ea0d31e98a499d5f56265d6d2c
Luis Fernando <luisfernando20140@outlook.com>
2021-05-07 02:29:12 -0300

    up

--- Sabores.cs
+++
@@ -1,9 +0,0 @@
-namespace DesafioDojo
-{
-    public class Sabores
-    {
-        public string Nome { get; set; }
-        public string Sabor { get; set; }
-        public int Nota { get; set; }
-    }
-}
